d9c168539aef8dfe72574cadc5f92b3c20fa84bf
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2025-01-09 15:03:25 -0300

    (HOME): Finish people section

--- src/pages/Home/components/IconButton.vue
+++ src/pages/Home/components/IconButton.vue
@@ -10,7 +10,7 @@ export default {
       type: String,
       required: true
     },
-    description: {
+    text: {
       type: String,
       required: true
     }
@@ -19,11 +19,13 @@ export default {
 </script>
 
 <template>
-  <div class="border border-primary p-2 flex items-center">
-    <component :is="icon" class="mr-2" />
-    <div>
-      <h3 class="font-bold">{{ title }}</h3>
-      <p>{{ description }}</p>
+    <div class="flex flex-col items-center cursor-pointer group">
+        <div class="bg-white border-2 border-primary rounded size-44 justify-center items-center flex group-hover:border-gray-500">
+            <component :is="icon" class="w-20 h-20 mx-auto mb-4 text-primary group-hover:text-gray-500" />
+        </div>
+        <div clas="text-center">
+              <h3 class="text-2xl text-primary group-hover:underline">{{ title }}</h3>
+              <p class="text-primary text-sm group-hover:underline">{{ text }}</p>
+        </div>
     </div>
-  </div>
 </template>
--- src/pages/Home/sections/People.vue
+++ src/pages/Home/sections/People.vue
@@ -1,17 +1,31 @@
 <script lang="ts">
 
+import { personsIconButtons } from '@/utilities/Buttons.utility';
+import IconButton from '../components/IconButton.vue';
+
 export default {
     name: 'People',
+    components: {
+        IconButton
+    },
+    setup () {
+        return {
+            personsIconButtons
+        }
+    }
 }
 
 </script>
 
 <template>
     <section id="people-section" class="w-full h-full text-center p-12">
-        <h1 class="text-2xl p-2">PERSONAS</h1>
+        <h1 class="text-3xl p-2 text-gray-700">PERSONAS</h1>
         <p class="text-sm">En Provincia Seguros tenemos diferentes propuestas para cada individuo.</p>
-        <div class="gid grid-cols-1 items-center">
-            
+        <div class="grid grid-cols-1 gap-4 px-40 pt-12 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <div v-for="button in personsIconButtons" :key="button.title" class="flex items-center">
+                <IconButton :icon="button.icon" :title="button.title" :text="button.text" />
+            </div>
         </div>
+
     </section>
 </template>
